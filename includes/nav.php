e99043fc82d691bff163a97c41e6767b7afa26eb
hanakin <midaym@gmail.com>
2014-01-18 11:42:30 +0100

    mock up some of the front page
    + index
    + dynamic primary nav
    + add dynamic side navigation

---
+++ includes/nav.php
@@ -0,0 +1,40 @@
+            <div class="grid">
+                <nav class="push grid-row list-inline">
+                    <li class="active grid-s-3 text-center">
+                        <a class="pull-left sidenav-btn" href="#" title="Menu"><span class="icon icon-reorder"></span>  <span class="badge">7</span></a>
+                    </li>
+                    <li class="active grid-s-6 text-center">
+                        <a class="navbar-brand brand" href="/" title="PHPBB | Creating Communities"><img class="brand-logo" src="assets/img/logo.png" alt="PHPBB | Creating Communities"> <span class="sr-only">PHPBB</span></a>
+                    </li>
+                    <li class="active grid-s-3 text-center">
+                        <a class="pull-right" href="#" title="Logout"><span class="octicons octicons-log-out"></span> <span class="sr-only">Logout</span> </a>
+                    </li>
+                </nav>    
+            </div>
+                <!-- 
+                <hgroup class="row hidden-sm">
+                    <h1 class="header-logo col-lg-2 col-sm-3"><a href="#"><img src="img/logo.png" height="40px" alt="PHPBB | Creating Communities" /><span class="text-hide">PHPBB</span></a></h1>
+                    <form action="" class="header-form form hidden-sm col-lg-3 col-sm-3">
+                            <input type="text" class="search form-control input-sm" placeholder="Search... " name="Search" title="Search" />
+                    </form>
+                    <nav class="header-nav col-lg-7 col-sm-6">
+                        <ul class="list-inline row">
+                            <li class="pull-left">
+                                <ul class="list-inline">
+                                    <li class="pull-left"><a href="#" title="Advanced Search"><span class="octicons octicons-list-alt"></span> <span class="text-hide">Advanced Search</span> </a></li>
+                                </ul>
+                            </li>
+                            <li class="pull-right">
+                                <ul class="list-inline">
+                                    <li class="header-avatar pull-left"><a href="#" title="Control Panel"><img src="img/user.jpg" width="30px" height="30px" title="Control Panel" alt="Control Panel" /></a> <span class="user">hanakin</span></li>
+                                    <li><a href="#" title="Notifications"><span class="octicons octicons-inbox"></span> <span class="text-hide">Notifications</span> <span class="badge">5</span></a></li>
+                                    <li><a href="#" title="Private Messages"><span class="octicons octicons-envelope"></span> <span class="text-hide">PMs</span> <span class="badge">2</span> </a></li>
+                                    <li><a href="#" title="Logout"><span class="octicons octicons-off"></span> <span class="text-hide">Logout</span> </a></li>
+                                    <li><a href="#" title="Moderator Control Panel"><span class="octicons octicons-cog"></span> <span class="text-hide">Moderator Control Panel</span> </a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </hgroup>
+            </div>
+                 -->
