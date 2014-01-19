29d35f0f67b569c232f196cb0a50fb7e3544bff2
hanakin <midaym@gmail.com>
2014-01-20 00:44:00 +0100

    fix class names for forums and remove recent title

--- includes/forum-row.php
+++ includes/forum-row.php
@@ -4,16 +4,15 @@
             <h2 class="forum-title h5"><a href="#">General Development Discussion</a> <span class="label red">LOCKED</span></h2>
             <dfn class="forum-description"><span>A link back to the support forum at www.phpbb.com.</span></dfn>
         </dt>
-            <p class="froum-stats-topics"><dfn class="hidden-s">Topics: </dfn><b>575</b></p>
-            <p class="froum-stats-posts"><dfn class="hidden-s">Posts: </dfn><b>348</b></p>
         <dd class="forum-stats label-group grid-m-2">
+            <p class="forum-stats-topics"><dfn class="hidden-s">Topics: </dfn><b>575</b></p>
+            <p class="forum-stats-posts"><dfn class="hidden-s">Posts: </dfn><b>348</b></p>
         </dd>
-            <img class="froum-recent-avatar pull-left" src="assets/img/user.jpg" alt="" />
-            <dl class="froum-recent-info pull-right">
-                <dt class="froum-recent-name text-truncate">Text...</dt>
-                <dd class="froum-recent-user admin">hanakin</dd>
-                <dd class="froum-recent-time"><time>22 minutes ago</time></dd>
         <dd class="forum-recent hidden-s grid-m-3 grid-l-2">
+            <img class="forum-avatar pull-left" src="assets/img/user.jpg" alt="" />
+            <dl class="forum-recent-info pull-left">
+                <dt class="forum-recent-user"><a class="admin" href="#">hanakin</a></dt>
+                <dd class="forum-recent-time"><time>22 minutes ago</time></dd>
             </dl>
         </dd>
     </dl>
