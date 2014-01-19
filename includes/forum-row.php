f936aacea1cd7645a4d7b908c17e52f6bfddf5a5
hanakin <midaym@gmail.com>
2014-01-20 00:42:56 +0100

    assign grid classes

--- includes/forum-row.php
+++ includes/forum-row.php
@@ -1,19 +1,19 @@
 <li class="forum-row grid-row">
-        <dt class="forum-header">
             <h2 class="forum-title h5"><a href="#">General Development Discussion</a></h2>
     <dl>
+        <dt class="forum-header grid-m-7 grid-l-8">
             <dfn class="forum-description"><span>A link back to the support forum at www.phpbb.com.</span></dfn>
         </dt>
-        <dd class="forum-stats label-group">
             <p class="froum-stats-topics"><dfn class="hidden-s">Topics: </dfn><b>575</b></p>
             <p class="froum-stats-posts"><dfn class="hidden-s">Posts: </dfn><b>348</b></p>
+        <dd class="forum-stats label-group grid-m-2">
         </dd>
-        <dd class="froum-recent hidden-s">
             <img class="froum-recent-avatar pull-left" src="assets/img/user.jpg" alt="" />
             <dl class="froum-recent-info pull-right">
                 <dt class="froum-recent-name text-truncate">Text...</dt>
                 <dd class="froum-recent-user admin">hanakin</dd>
                 <dd class="froum-recent-time"><time>22 minutes ago</time></dd>
+        <dd class="forum-recent hidden-s grid-m-3 grid-l-2">
             </dl>
         </dd>
     </dl>
