05d0cc5855d371ecbe68916517cc1e7afd11f80c
hanakin <midaym@gmail.com>
2014-01-19 21:00:56 +0100

    rework wrapping of primary content

--- index.php
+++ index.php
@@ -22,44 +22,12 @@
                 <?php include("includes/nav.php"); ?>
             </header>
             <?php include("includes/sidenav.php"); ?>
-            <div class="inner-wrap">
-                <?php include("includes/breadcrumbs.php"); ?>
-                <div class="content">
-                    
-                    <h1>Treasure Island</h1>
-                    
-                    <p><a name="id380362"><!--anchor--></a>SQUIRE TRELAWNEY, Dr. Livesey, and the rest of these gentlemen having
-                    asked me to write down the whole particulars about Treasure Island, from
-                    the beginning to the end, keeping nothing back but the bearings of the
-                    island, and that only because there is still treasure not yet lifted, I
-                    take up my pen in the year of grace 17__ and go back to the time when
-                    my father kept the Admiral Benbow inn and the brown old seaman with the
-                    sabre cut first took up his lodging under our roof.</p>
-                    <p><a name="id380370"><!--anchor--></a>I remember him as if it were yesterday, as he came plodding to the
-                    inn door, his sea-chest following behind him in a hand-barrow—a
-                    tall, strong, heavy, nut-brown man, his tarry pigtail falling over the
-                    shoulder of his soiled blue coat, his hands ragged and scarred, with
-                    black, broken nails, and the sabre cut across one cheek, a dirty, livid
-                    white. I remember him looking round the cover and whistling to himself
-                    as he did so, and then breaking out in that old sea-song that he sang so
-                    often afterwards:</p>
-                    <p><a name="id380380"><!--anchor--></a>in the high, old tottering voice that seemed to have been tuned and
-                    broken at the capstan bars. Then he rapped on the door with a bit of
-                    stick like a handspike that he carried, and when my father appeared,
-                    called roughly for a glass of rum. This, when it was brought to him,
-                    he drank slowly, like a connoisseur, lingering on the taste and still
-                    looking about him at the cliffs and up at our signboard.</p>
-                    <p><a name="id380387"><!--anchor--></a>"This is a handy cove," says he at length; "and a pleasant sittyated
-                    grog-shop. Much company, mate?"</p>
-                    <p><a name="id380391"><!--anchor--></a>My father told him no, very little company, the more was the pity.</p>
-                    <p><a name="id380395"><!--anchor--></a>"Well, then," said he, "this is the berth for me. Here you, matey," he
-                    cried to the man who trundled the barrow; "bring up alongside and help
-                    up my chest. I'll stay here a bit," he continued. "I'm a plain man; rum
-                    and bacon and eggs is what I want, and that head up there for to watch
-                    ships off. What you mought call me? You mought call me captain. Oh, I
-                    see what you're at—there"; and he threw down three or four gold pieces
-                    on the threshold. "You can tell me when I've worked through that," says
-                    he, looking as fierce as a commander.</p>
+            <div class="outer-wrap">
+                <section class="breadcrumbs-wrap grid">
+                    <?php include("includes/breadcrumbs.php"); ?>
+                </section>
+                <div class="inner-wrap content">
+
                     <?php include("includes/forumlist.php"); ?>
                     
                 </div>
