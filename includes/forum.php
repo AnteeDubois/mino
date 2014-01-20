f8c381d3f315daa286cec3e0714c556f4954de6d
hanakin <midaym@gmail.com>
2014-01-20 23:45:49 +0100

    setup gravatar testing

--- includes/forum.php
+++ includes/forum.php
@@ -6,8 +6,9 @@
             $status = ' <span class="label red">LOCKED</span>';
             $desc = 'General discussion of development ideas and the approaches taken in the 3.x branch of phpBB. The next feature release of phpBB 3 will be 3.1/Ascreaus followed by 3.2/Arsia.';
             $topics = '623';
-            $posts = '6439';
-            $usertype = 'adimn';
+            $posts = '6439';            
+            $avatar = 'http://www.gravatar.com/avatar/' . md5(strtolower(trim("midaym@gmail.com")));
+            $usertype = 'admin';
             $user = 'hanakin';
             $time = '22 mins ago';
         ?>
@@ -18,6 +19,7 @@
             $desc = 'These requests for comments have lead to an implemented feature that has been successfully merged into the 3.1/Ascraeus branch. Everything listed in this forum will be available in phpBB 3.1.';
             $topics = '100';
             $posts = '3300';
+            $avatar = 'assets/img/user.png';
             $usertype = 'mod';
             $user = 'JonnyB';
             $time = 'Sun Jan 12, 2014 8:27 pm';
@@ -29,6 +31,7 @@
             $desc = 'Discuss general development subjects that are not specific to a particular version like the versioning control system we use or other infrastructure.';
             $topics = '200';
             $posts = '1398';
+            $avatar = 'assets/img/user.png';
             $usertype = 'user';
             $user = 'bojangles';
             $time = 'Today, 3:23 PM';
