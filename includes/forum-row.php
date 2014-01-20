f8334c7ff3baaddd999e08b6f104b6e6d6f5363b
hanakin <midaym@gmail.com>
2014-01-20 12:40:33 +0100

    change structure of additional cells for forum row to be a combined cell this way we have more flexbility with our grid

--- includes/forum-row.php
+++ includes/forum-row.php
@@ -4,16 +4,19 @@
             <h2 class="forum-title h5"><a href="#"><?php echo $title; ?></a><?php echo $status; ?></h2>
             <dfn class="forum-description"><?php echo $desc; ?></dfn>
         </dt>
-        <dd class="forum-stats label-group grid-m-2">
-        </dd>
-        <dd class="forum-recent hidden-s grid-m-3 grid-l-2">
-            <img class="forum-avatar pull-left" src="assets/img/user.jpg" alt="" />
-            <dl class="forum-recent-info pull-left">
-            </dl>
+        <dd class="forum-info grid-m-6 grid-l-5 grid-w-4">
+            
+            <div class="forum-stats label-group pull-left">
                 <p class="forum-stats-topics"><dfn class="hidden-s">Topics: </dfn><b><?php echo $topics; ?></b></p>
                 <p class="forum-stats-posts"><dfn class="hidden-s">Replies: </dfn><b><?php echo $posts; ?></b></p>
+            </div>
+            <div class="forum-recent pull-left hidden-s">
+                <img class="forum-avatar pull-left" src="assets/img/user.jpg" alt="" />
+                <dl class="forum-recent-info pull-left">
                     <dt class="forum-recent-user"><a class="<?php echo $usertype; ?>" href="#"><?php echo $user; ?></a></dt>
                     <dd class="forum-recent-time"><time><?php echo $time; ?></time></dd>
+                </dl>
+            </div>
         </dd>
     </dl>
 </li>
