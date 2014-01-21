979fd050df78b146db703185c11e1dc695b34421
hanakin <midaym@gmail.com>
2014-01-21 22:40:43 +0100

    change some colors in forms and rename forums-forum to forum

--- includes/forum.php
+++ includes/forum.php
@@ -1,9 +1,9 @@
-<li class="forums-forum">
+<li class="forum">
     <h2 class="forum-name h4"><a href="#" title="FORUM"><?php echo $forum; ?></a></h2>
     <ol class="forum-list list-unstyled">
         <?php
             $title = '[3.x] Discussion';
-            $status = ' <span class="label red">LOCKED</span>';
+            $status = ' <span class="label pomegranate">LOCKED</span>';
             $desc = 'General discussion of development ideas and the approaches taken in the 3.x branch of phpBB. The next feature release of phpBB 3 will be 3.1/Ascreaus followed by 3.2/Arsia.';
             $topics = '623';
             $posts = '6439';            
@@ -15,7 +15,7 @@
         <?php include("includes/forum-row.php"); ?>
         <?php
             $title = '[3.1/Ascraeus] Merged RFCs';
-            $status = ' <span class="label green">NEW</span>';
+            $status = ' <span class="label nephritis">NEW</span>';
             $desc = 'These requests for comments have lead to an implemented feature that has been successfully merged into the 3.1/Ascraeus branch. Everything listed in this forum will be available in phpBB 3.1.';
             $topics = '100';
             $posts = '3300';
