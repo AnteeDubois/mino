92ae81739e3ef6f34086e1146699d6afd6108f39
hanakin <midaym@gmail.com>
2014-01-25 13:03:10 +0100

    refactor of index include tooltips js

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
         <link rel="stylesheet" href="assets/css/style.css">
-
+        
+        <script src="assets/js/vendor/html5-3.6-respond-1.1.0.min"></script>
        <!--  <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script> -->
        <!-- <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js"></script> -->
        <!-- <script>window.jQuery || document.write('<script src="assets/js/vendor/modernizr-ck.js"><\/script>')</script> -->
@@ -29,22 +30,22 @@
                 <div class="content inner-wrap">
 
                     <?php include("includes/forumlist.php"); ?>
+                    <?php include("includes/info.php"); ?>
                     
                 </div>
-                <footer class="footbar">
-                    <section class="footbar-wrap grid">
-                        <?php include("includes/info.php"); ?>
-                        <?php include("includes/copyright.php"); ?>
-                    </section>                        
-                </footer>
             </div>
+            <footer class="footbar">
+                <section class="footbar-wrap grid">                    
+                    <?php include("includes/copyright.php"); ?>
+                </section>                        
+            </footer>
             
         <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
        <!--  // <script src="//code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.js"></script> -->
         
         <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="assets/js/vendor/tooltip.js"></script>
         <script src="assets/js/vendor/main.js"></script>
-
         <script>
             var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
             (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
