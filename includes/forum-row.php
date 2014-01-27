40b45aeb5b3b7ad12cf82c7360c6991d90cf1878
hanakin <midaym@gmail.com>
2014-01-27 19:40:13 +0100

    since we are reusing code from forum row lets separate it into its own object

--- includes/forum-row.php
+++ includes/forum-row.php
@@ -4,17 +4,16 @@
             <h2 class="forum-title h5"><?php echo $status; ?><a href="#"><?php echo $title; ?></a><?php echo $label; ?></h2>
             <dfn class="forum-description"><?php echo $desc; ?></dfn>
         </dt>
-        <dd class="forum-info grid-m-6 grid-l-5 grid-w-4">
-            
-            <div class="forum-stats label-group pull-left">
-                <p class="forum-stats-topics"><dfn class="hidden-s">Topics: </dfn><b><?php echo $topics; ?></b></p>
-                <p class="forum-stats-posts"><dfn class="hidden-s">Replies: </dfn><b><?php echo $posts; ?></b></p>
+        <dd class="forum-details details grid-m-6 grid-l-5 grid-w-4">
+            <div class="details-stats label-group pull-left">
+                <p class="forum-stat"><dfn class="hidden-s">Topics: </dfn><b><?php echo $topics; ?></b></p>
+                <p class="forum-stat"><dfn class="hidden-s">Replies: </dfn><b><?php echo $posts; ?></b></p>
             </div>
-            <div class="forum-recent pull-left hidden-s">
-                <img class="forum-avatar img-circle pull-left" src="<?php echo $avatar; ?>" alt="" />
-                <dl class="forum-recent-info pull-left">
-                    <dt class="forum-recent-user"><a class="<?php echo $usertype; ?>" href="#"><?php echo $user; ?></a></dt>
-                    <dd class="forum-recent-time"><time><?php echo $time; ?></time></dd>
+            <div class="details-recent pull-left hidden-s">
+                <img class="details-avatar img-circle pull-left" src="<?php echo $avatar; ?>" alt="" />
+                <dl class="details-info pull-left">
+                    <dt class="details-user"><a class="<?php echo $usertype; ?>" href="#"><?php echo $user; ?></a></dt>
+                    <dd class="details-time"><time><?php echo $time; ?></time></dd>
                 </dl>
             </div>
         </dd>
